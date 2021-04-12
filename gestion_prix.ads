feb23c68b0db07ccbc826ef3cde9e2d9ae68405b
Maxime <monillon@outlook.fr>
2021-04-12 18:42:01 +0200

    Initialisation des T_prix a 0,0

--- gestion_prix.ads
+++ gestion_prix.ads
@@ -5,7 +5,7 @@ use ada.text_io, ada.integer_text_io, ada.float_text_io, outils;
 package gestion_prix is
 
    type T_prix is record
-      ecu, galion : integer;
+      ecu, galion : integer := 0;
    end record;
 
 
